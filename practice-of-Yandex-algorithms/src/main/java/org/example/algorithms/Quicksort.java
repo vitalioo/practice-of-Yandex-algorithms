cdfd604f0e476a3b75f9983d2267b88cbbf7c0b2
v.avdeev <v.avdeev@g.nsu.ru>
2023-11-15 22:32:23 +0300

    Added Quicksort

---
+++ practice-of-Yandex-algorithms/src/main/java/org/example/algorithms/Quicksort.java
@@ -0,0 +1,45 @@
+package org.example.algorithms;
+
+import java.util.ArrayList;
+
+public class Quicksort {
+    public ArrayList<Integer> quicksort(ArrayList<Integer> list, int low, int high) {
+        if (low < high) {
+            int pivot = partition(list, low, high);
+
+            quicksort(list, low, pivot - 1);
+            quicksort(list, pivot, high);
+        }
+
+        return list;
+    }
+
+    public int partition(ArrayList<Integer> numbers, int low, int high) {
+        int middle = low + (high - low) / 2;
+        Integer pivot = numbers.get(middle);
+
+        int leftIndex = low;
+        int rightIndex = high;
+
+        while (leftIndex <= rightIndex) {
+
+            while (numbers.get(leftIndex) < pivot) {
+                ++leftIndex;
+            }
+
+            while (numbers.get(rightIndex) > pivot) {
+                --rightIndex;
+            }
+
+            if (leftIndex <= rightIndex) {
+                Integer tmpValue = numbers.get(leftIndex);
+                numbers.set(leftIndex, numbers.get(rightIndex));
+                numbers.set(rightIndex, tmpValue);
+                ++leftIndex;
+                --rightIndex;
+            }
+        }
+
+        return leftIndex;
+    }
+}
